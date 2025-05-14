851398682e06a359097b730ec84b3e695b505eaa
Alvin Zhu <167814328+SmartCat30@users.noreply.github.com>
2025-05-14 10:29:55 -0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,13 +4,13 @@ addLayer("p", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		bits: new Decimal(0),
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    resource: "energy", // Name of prestige currency
+    baseResource: "bits", // Name of resource prestige is based on
+    baseAmount() {return player.bits}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -22,13 +22,12 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "e", description: "E: Reset for energy", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
     upgrades: {
         11: {
-            title: "Make this whatever you want!",
-	    description: "Double your point gain.",
+	    description: "2x bits.",
 	    cost: new Decimal(1),
         },
     },
